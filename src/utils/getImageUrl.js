4af4de2e2518563bbc9f1772093552b7ac094f2a
Moshira89 <mushiraaljak@example.com>
2025-07-14 22:00:46 +0200

    updated image

--- src/utils/getImageUrl.js
+++ src/utils/getImageUrl.js
@@ -1,12 +1,12 @@
 const imageMap = {
-  'echidna.jpg': new URL('../assets/images/echidna.jpg', import.meta.url).href,
+  'echidna.jpg': new URL('../assets/images/Echidna.jpg', import.meta.url).href,
   'TasmanianDevil.jpg': new URL('../assets/images/TasmanianDevil.jpg', import.meta.url).href,
   'quokka.jpg': new URL('../assets/images/quokka.jpg', import.meta.url).href,
   'frill-neckedLizard.jpg': new URL('../assets/images/frill-neckedLizard.jpg', import.meta.url).href,
-  'hawksbill-turtle.jpg': new URL('../assets/images/hawksbill-turtle.jpg', import.meta.url).href,
+  'hawksbill-turtle.jpg': new URL('../assets/images/Hawksbill-turtle.jpg', import.meta.url).href,
   'Perentie.jpg': new URL('../assets/images/Perentie.jpg', import.meta.url).href,
   'cassowary.jpg': new URL('../assets/images/cassowary.jpg', import.meta.url).href,
-  'kookaburra.jpg': new URL('../assets/images/kookaburra.jpg', import.meta.url).href,
+  'kookaburra.jpg': new URL('../assets/images/Kookaburra.jpg', import.meta.url).href,
   'Yellow-tailedCockatoo.jpg': new URL('../assets/images/Yellow-tailedCockatoo.jpg', import.meta.url).href, // FIXED
   'placeholder.jpg': new URL('../assets/images/placeholder.jpg', import.meta.url).href
 };
