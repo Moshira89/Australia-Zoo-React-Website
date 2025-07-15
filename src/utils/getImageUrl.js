88cdc33ca8de5df1ac6ec3e5a697e85605b8eefb
Moshira89 <mushiraaljak@example.com>
2025-07-15 13:21:02 +0200

    fix: image

--- src/utils/getImageUrl.js
+++ src/utils/getImageUrl.js
@@ -3,9 +3,9 @@ const imageMap = {
   'TasmanianDevil.jpg': new URL('../assets/images/TasmanianDevil.jpg', import.meta.url).href,
   'quokka.jpg': new URL('../assets/images/quokka.jpg', import.meta.url).href,
   'frill-neckedLizard.jpg': new URL('../assets/images/frill-neckedLizard.jpg', import.meta.url).href,
-  'hawksbill-turtle.jpg': new URL('../assets/images/Hawksbill-turtle.jpg', import.meta.url).href,
+  'hawksbill-turtle.jpg': new URL('../assets/images/Hawksbill-Turtle.jpg', import.meta.url).href,
   'Perentie.jpg': new URL('../assets/images/Perentie.jpg', import.meta.url).href,
-  'cassowary.jpg': new URL('../assets/images/cassowary.jpg', import.meta.url).href,
+  'cassowary.jpg': new URL('../assets/images/Cassowary.jpg', import.meta.url).href,
   'kookaburra.jpg': new URL('../assets/images/Kookaburra.jpg', import.meta.url).href,
   'Yellow-tailedCockatoo.jpg': new URL('../assets/images/Yellow-tailedCockatoo.jpg', import.meta.url).href, // FIXED
   'placeholder.jpg': new URL('../assets/images/placeholder.jpg', import.meta.url).href
